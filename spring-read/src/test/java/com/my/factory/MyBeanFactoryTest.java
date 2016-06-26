2b245b0a4cc3ba72393f5cc1fba47f0f6049c75b
zengwei shao <zengwei.shao@rea-group.com>
2016-06-26 10:28:37 +0800

    make first test passed. zengwei shao

--- spring-read/src/test/java/com/my/factory/MyBeanFactoryTest.java
+++ spring-read/src/test/java/com/my/factory/MyBeanFactoryTest.java
@@ -1,18 +1,21 @@
 package com.my.factory;
 
+import org.junit.Before;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.MutablePropertyValues;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.support.RootBeanDefinition;
-import org.springframework.beans.factory.support.StaticListableBeanFactory;
 import org.springframework.beans.factory.xml.AbstractBeanFactoryTests;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.support.StaticApplicationContext;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.tests.sample.beans.LifecycleBean;
 import org.springframework.tests.sample.beans.TestBean;
-import org.springframework.util.MultiValueMap;
+import org.springframework.tests.sample.beans.factory.DummyFactory;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by zwshao on 6/25/16.
@@ -20,23 +23,37 @@ import java.util.HashMap;
 public class MyBeanFactoryTest extends AbstractBeanFactoryTests {
 
 
-    @Override
-    protected BeanFactory getBeanFactory() {
+    private DefaultListableBeanFactory applicationContext;
+
+
+    protected DefaultListableBeanFactory createContext() throws Exception {
         StaticApplicationContext parent = new StaticApplicationContext();
 
-        HashMap<String, String> father = new HashMap<>();
-        father.put("name", "Albert");
-        parent.registerSingleton("father", TestBean.class, new MutablePropertyValues(father));
+        Map<String, String> m = new HashMap<>();
+        m.put("name", "Robric");
+        parent.registerPrototype("rod", TestBean.class, new MutablePropertyValues(m));
 
-        HashMap<String, String> rod = new HashMap<>();
-        rod.put("name", "rod");
-        parent.registerSingleton("rod", TestBean.class, new MutablePropertyValues(rod));
+        Map<String, Object> fatherProperties = new HashMap<>();
+        fatherProperties.put("name", "Albert");
+        fatherProperties.put("age", 31);
+        parent.registerSingleton("father", TestBean.class, new MutablePropertyValues(fatherProperties));
 
-        parent.registerSingleton("singletonFactory", TestBean.class);
-        new XmlBeanDefinitionReader(parent).loadBeanDefinitions(new ClassPathResource("test.xml"));
 
-        StaticApplicationContext staticApplicationContext = new StaticApplicationContext(parent);
 
-        return staticApplicationContext;
+        DefaultListableBeanFactory applicationContext = new DefaultListableBeanFactory(parent);
+        applicationContext.addBeanPostProcessor(new LifecycleBean.PostProcessor());
+        new XmlBeanDefinitionReader(applicationContext).loadBeanDefinitions(new ClassPathResource("test.xml"));
+
+        return applicationContext;
+    }
+
+    @Before
+    public void before() throws Exception {
+        this.applicationContext = createContext();
+    }
+
+    @Override
+    protected BeanFactory getBeanFactory() {
+        return applicationContext;
     }
 }
